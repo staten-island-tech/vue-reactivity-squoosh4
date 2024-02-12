72d3b5f81148f398ec69d409ec3e5f6ade7e7382
squoosh4 <110200988+squoosh4@users.noreply.github.com>
2024-02-12 13:13:54 -0500

    wgr
    wewe

--- fake-ahh-forum/README.md
+++ fake-ahh-forum/README.md
@@ -1,4 +1,4 @@
-# block-guessr
+# fake-ahh-forum
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ fake-ahh-forum/src/views/ForumView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="item">
+        <div class="details">
+          <h3>
+            <slot name="title"></slot>
+          </h3>
+          <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
